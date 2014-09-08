d713d6780754d8587a9910279d4a45c5dda88211
am4nj0t <1kaur.amanjot@gmail.com>
2014-09-08 20:03:38 +0000

    Fixed syntax error.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -14,7 +14,7 @@ class PostsController < ApplicationController
   def create
   	@post = Post.new(params.require(:post).permit(:title, :body))
   	if @post.save
-  		flash.[:notice] = "Post was saved"
+  		flash[:notice] = "Post was saved"
   		redirect_to @post
   	else
   		flash[:error] = "There was an error saving the post. Please try again."
