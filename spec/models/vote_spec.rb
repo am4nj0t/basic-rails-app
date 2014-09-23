9039d6ee327497fbb20fbc1e2888027cb6a9fa8c
am4nj0t <1kaur.amanjot@gmail.com>
2014-09-23 17:54:41 +0000

    Included test module helpers and fixed specs

--- spec/models/vote_spec.rb
+++ spec/models/vote_spec.rb
@@ -2,6 +2,8 @@ require 'rails_helper'
 
 describe Vote do
 
+	include TestFactories
+
 	describe "validations" do
 
   		before do
@@ -19,6 +21,15 @@ describe Vote do
 			it "only allows 1 as a value" do
 				expect(@second_vote.value).to eq(1)
 			end
+		end	
+	end	
+
+	describe 'after_save' do
+		it "calls `Post#update_rank` after save" do
+			post = associated_post
+			vote = Vote.new(value: 1, post: post)
+			expect(post).to receive(:update_rank)
+			vote.save 
 		end
-	end					
+	end							
 end
